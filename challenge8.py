13663afcfe041d5f0edb6da7161c9afe3091f1a3
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-04-20 18:38:52 -0400

    Create challenge8.py

---
+++ challenge8.py
@@ -0,0 +1,17 @@
+# Makes an equalateral triangle
+
+# Challenge #8
+
+import turtle
+
+window = turtle.Turtle() # Opens the window
+
+shub = turtle.Turtle() # Name
+
+# Moves the turtle
+
+shub.forward(174)
+shub.left(120)
+shub.forward(174)
+shub.left(120)
+shub.forward(174)
