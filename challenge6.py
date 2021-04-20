318c70c2c2dc6d27588ede618ee0c64832c1623b
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-04-20 18:37:23 -0400

    Create challenge6.py

---
+++ challenge6.py
@@ -0,0 +1,23 @@
+# Makes a square with Turtle 
+
+# Challenge #6
+
+import turtle
+
+window = turtle.Turtle() # Opens the window
+
+shub = turtle.Turtle() # Name
+
+# Moves the turtle
+
+shub.forward(200)
+shub.left(90)
+shub.forward(200)
+shub.left(90)
+shub.forward(200)
+shub.left(90)
+shub.forward(200)
+
+# Keeps the window open
+
+turtle.getscreen()._root.mainloop()
