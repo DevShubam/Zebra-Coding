a5f3f0f709805b2079b5cf966ef27acc38527261
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-04-20 18:37:59 -0400

    Create challenge7.py

---
+++ challenge7.py
@@ -0,0 +1,21 @@
+# Makes a large rectangle
+
+# Challenge #7
+
+import turtle
+
+window = turtle.Turtle() # Opens the window
+
+shub = turtle.Turtle() # Name
+
+# Moves the turtle
+
+shub.forward(800)
+shub.left(90)
+shub.forward(1500)
+shub.left(90)
+shub.forward(800)
+shub.left(90)
+shub.forward(1500)
+
+turtle.getscreen()._root.mainloop()
