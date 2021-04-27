0dbaec14ee20d8304dba9b99bdb285ef2354a12d
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-04-27 18:52:40 -0400

    Create challenge11.py

---
+++ challenge11.py
@@ -0,0 +1,26 @@
+# This asks the user questions. You then answer them.
+# At the end it will tell you what you answered.
+
+birth = input("Where were you born?")
+
+age = input("How old are you?")
+
+iceCream = input("What's your favourite ice cream flavor?")
+
+movie = input("What's your favourite movie?")
+
+colour = input("What's your favourite colour?")
+
+###
+
+print("-----------------------------------------") # Creates seperation between the questions and answers
+
+print("Here is what I have learned")
+
+print("You were born in",birth,"and you are currently",age,"years old.")
+
+print("You like eating",iceCream,"flavored ice cream.")
+
+print("We should sit down and watch the",movie,"together.")
+
+print("Finally, your favourite color is",colour)
