599d0d4f2f63ccd5018cf778a29220548e9fea4c
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-05-04 18:21:39 -0400

    Create challenge16.py

---
+++ challenge16.py
@@ -0,0 +1,9 @@
+increment = 1.15
+
+item1 = int(input("What was the price of your first item?"))
+
+item2 = int(input("What was the price of your second item?"))
+
+item3 = int(input("What was the price of your third item?"))
+
+print("The total of your items with tax is: " + str(item1 + item2 + item3 * 1.15))
