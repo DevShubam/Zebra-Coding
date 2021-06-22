6eb933bb7f6383bfc881b498355a0d227e587451
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-06-22 17:25:45 -0400

    Update challenge26.py

--- challenge26.py
+++ challenge26.py
@@ -1,15 +1,3 @@
-###############
-#  Not Done   #
-###############
-
-# Extend challenge 25 to give more information to the user.
-
-# Ask them to guess the number 3 times (we have not learned repeated loops yet so you will have the same code repeated 3 times.
-
-# If the user’s guess is less than the actual number, tell the user “The number is less than your guess”
-# If the user’s guess is greater than the actual number, tell the user “The number is greater than your guess”
-# If the users guess equals the random number, tell the user “You guessed it right!”
-
 import random
 
 random_num = random.randint(1,5)
@@ -17,7 +5,54 @@ random_num = random.randint(1,5)
 
 print(random_num)
 
-userNum = int(input("Pick a number between 1 and 5."))
+print("Let's play a guessing game, I am thinking of a number that is inbetween 1 and 5, you need to guess this number correctly. "
+"You have three tries to guess it")
+
+userNum = int(input("What is your guess: "))
+
+# To prevent guesses that are not inbetween 1 and 5
+
+if userNum <= 0:
+    print("Your number must be larger than 0.")
+    exit()
+
+elif userNum >= 6:
+    print("Your number must be under 5")
+    exit()
+
+# For Guess 1
 
 if userNum == random_num:
     print("Congrats, you picked the right number.")
+
+elif userNum < random_num:
+    print("Your guess was too small, try again")
+
+else:
+    print("Your guess was too large, try again.")
+
+userNum = int(input("What is your guess: "))
+
+# For guess 2
+
+if userNum == random_num:
+    print("Congrats, you picked the right number.")
+
+elif userNum < random_num:
+    print("Your guess was too small, try again")
+
+else:
+    print("Your guess was too large.")
+
+userNum = int(input("What is your guess: "))
+
+# Guess 3
+
+if userNum == random_num:
+    print("Congrats, you picked the right number.")
+
+elif userNum < random_num:
+    print("Your guess was too small, try again")
+
+else:
+    print("Your guess was too large.")
