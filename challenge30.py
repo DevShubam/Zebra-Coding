01adaf502975947c117d12dcf426ca1575c44642
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-08-10 16:35:56 -0400

    Create challenge30.py

---
+++ challenge30.py
@@ -0,0 +1,5 @@
+# Accept a number, n, from the user and calculate the sum of all numbers between 1 and n including n.
+
+# For example, if user gives 10 as an input, the output should be 55
+
+int(input("Give me a number: "))
