e0b07e07429f1f5d6adcefefdd8c96df7c7975ca
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-08-10 17:55:49 -0400

    finished ;D

--- challenge30.py
+++ challenge30.py
@@ -2,4 +2,12 @@
 
 # For example, if user gives 10 as an input, the output should be 55
 
-int(input("Give me a number: "))
+n = int(input("Give me a number: "))
+
+counter = 0
+sum = 0
+while counter <= n:
+    sum += counter
+    counter += 1
+print(sum)
+
