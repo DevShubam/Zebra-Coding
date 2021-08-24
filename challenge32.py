48ef46ce3c9b1953f14573cc21fcdeb9d3544ddb
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-08-24 17:36:54 -0400

    Create challenge32.py

---
+++ challenge32.py
@@ -0,0 +1,7 @@
+# Write a program to get a word from the user and display all of its characters separately.
+word = str(input("What is your word: "))
+
+for n in word:
+  print(n)
+  
+# 'n' can be changed to anything.
