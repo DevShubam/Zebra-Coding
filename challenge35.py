57b712868349098a76080358fcf5e7e2a8f24cbd
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-11-17 17:58:04 -0500

    Create challenge35.py

---
+++ challenge35.py
@@ -0,0 +1,9 @@
+
+# Get an input number from the user and print its multiplication table up till 20.
+
+# For example: 2 goes all the way up to 20
+
+user_num = int(input("Enter your number: "))
+
+for i in range(0, 21, user_num):
+    print(i)
